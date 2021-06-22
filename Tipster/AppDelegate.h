025a7f682e4ad8dc5ba423b1494da0443d9f29e8
Samantha Burak <samanthaburak@fb.com>
2021-06-22 15:56:59 -0400

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by samanthaburak on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
